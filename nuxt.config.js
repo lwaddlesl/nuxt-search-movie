d87e12cdcee5a00c0bf5a1c2f9b912b4ee960298
nekit <n>
2021-10-19 22:38:03 +0300

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/nuxt-search-movie/'
+  },
   head: {
     titleTemplate: '%s - SearchMovies',
     title: 'SearchMovies',
