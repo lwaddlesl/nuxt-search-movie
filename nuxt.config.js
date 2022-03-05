53a9e6a61b4c3aa0b7adc2318579960650fd57b8
Nikita <nekit.zernov@yandex.ru>
2022-03-05 21:33:50 +0300

    add gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
+  router: {
+    base: '/nuxt-search-movie/'
+  },
   head: {
     titleTemplate: '%s - SearchMovies',
     title: 'SearchMovies',
